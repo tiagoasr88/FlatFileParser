80ed0fdbe278eb626525e37f798118d1b92b77fe
Tiago Rossetto <tiago.rossetto@logicalminds.com.br>
2021-11-21 15:08:16 -0300

    Init

---
+++ src/FlatFileParser/Attributes/FixedLengthFieldAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlatFileParser.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class FixedLengthFieldAttribute : Attribute
+    {
+        public int StartPosition { get; set; }
+        public int Length { get; set; }
+        public string Format { get; set; }
+
+        public FixedLengthFieldAttribute(int startPosition, int length)
+        {
+            StartPosition = startPosition;
+            Length = length;
+            Format = String.Empty;
+        }
+
+        public FixedLengthFieldAttribute(int startPosition, int length, string format)
+        {
+            StartPosition = startPosition;
+            Length = length;
+            Format = format;
+        }
+    }
+}
---
+++ src/FlatFileParser/Parsers/FixedLengthParser.cs
@@ -0,0 +1,53 @@
+﻿using FlatFileParser.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace FlatFileParser.Parsers
+{
+    public class FixedLengthParser
+    {
+        public T ParseLine<T>(string line) where T : class
+        {
+            var obj = (T)Activator.CreateInstance(typeof(T));
+            var objProperties = typeof(T).GetProperties();
+            foreach (var property in objProperties)
+            {
+                var attr = (FixedLengthFieldAttribute)property.GetCustomAttributes(typeof(FixedLengthFieldAttribute), false).First();
+
+                var ret = ReadPositionalFileField(line, attr.StartPosition, attr.Length);
+
+                property.SetValue(obj, DynamicConvert(ret, property.PropertyType, attr.Format));
+            }
+            return obj;
+        }
+
+        private string ReadPositionalFileField(string line, int startPosition, int length)
+        {
+            return line.Substring(startPosition, length);
+        }
+
+        private object DynamicConvert(string value, Type type, string format)
+        {
+            if (type == typeof(DateTime))
+            {
+                if (string.IsNullOrWhiteSpace(format))
+                    return DateTime.Parse(value);
+                else
+                    return DateTime.ParseExact(value, format, CultureInfo.InvariantCulture);
+            }
+
+            if (type == typeof(Decimal))
+            {
+                if (!string.IsNullOrWhiteSpace(format))
+                    return decimal.Parse(value, new CultureInfo(format));
+                else
+                    return decimal.Parse(value, CultureInfo.InvariantCulture);
+            }
+
+            return Convert.ChangeType(value, type);
+        }
+    }
+}
